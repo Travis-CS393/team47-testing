6e9deb0e00562f6c651dca37cdd3d89c8cc1ec1d
Daniel Bang <daniel.bang907@gmail.com>
2019-11-12 14:33:46 -0600

    player

--- Deliverables/7/7.1/test_driver.py
+++ Deliverables/7/7.1/test_driver.py
@@ -67,18 +67,18 @@ if __name__ == "__main__":
 
          for input in objs[2:]:
             if not game_terminated:
-               #if valid_move_input(input):
-               client_socket.sendall(bytes(json.dumps(input), "utf-8"))
-               ret_val = client_socket.recv(8192)
-               if ret_val.decode("utf-8") == "no name" or ret_val.decode("utf-8") == "None":
-                  output.append("GO has gone crazy!")
-                  break
+               if valid_move_input(input):
+                  client_socket.sendall(bytes(json.dumps(input), "utf-8"))
+                  ret_val = client_socket.recv(8192)
+                  if ret_val.decode("utf-8") == "no name" or ret_val.decode("utf-8") == "None":
+                     output.append("GO has gone crazy!")
+                     break
+                  else:
+                     output.append(ret_val.decode("utf-8"))
                else:
-                  output.append(ret_val.decode("utf-8"))
-               #else:
-               #   output.append("GO has gone crazy!")
-               #   game_terminated = True
-               #   break
+                  output.append("GO has gone crazy!")
+                  game_terminated = True
+                  break
             else:
                break
 
