77e70579f97e0a937ee9ddaa56714a42275aa154
JennyTL8 <jennytl8@github.com>
2019-11-26 13:43:08 -0600

    fix bananas

--- Deliverables/8/8.1/remote_player_proxy.py
+++ Deliverables/8/8.1/remote_player_proxy.py
@@ -29,8 +29,16 @@ class RemotePlayerProxy():
 
 	def receive_stone(self, stone_type):
 		self.connection.sendall(bytes(json.dumps(["receive-stones", make_stone(stone_type).get_raw()]), "utf-8"))
-		response = self.connection.recv(8192)
-		return response.decode("utf-8")
+		response = None
+		try:
+			response = self.connection.recv(8192)
+		except:
+			pass
+		
+		if not response:
+			return response
+		else:
+			return response.decode("utf-8")
 
 	def choose_move(self, boards):
 		self.connection.sendall(bytes(json.dumps(["make-a-move", format_board(boards)]), "utf-8"))
