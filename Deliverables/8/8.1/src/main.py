84d40d01d8d5c353137b6e6dd4b27b66b8086a78
JennyTL8 <jennytl8@github.com>
2019-12-11 16:27:11 -0600

    pather

--- Deliverables/8/8.1/src/main.py
+++ Deliverables/8/8.1/src/main.py
@@ -2,16 +2,16 @@ import sys, json, socket
 sys.path.append('./src')
 from go_admin import GoAdmin
 
-#local player import
 go_config = json.load(open('go.config'))
+#remote player import
+HOSTNAME = go_config['IP']
+PORT = go_config['port']
+#local player import
 default_player_path = go_config['default-player']
 sys.path.append(default_player_path)
 from go_player_base import GoPlayerBase
 
-#remote player import
-go_config = json.load(open('go.config'))
-HOSTNAME = go_config['IP']
-PORT = go_config['port']
+
 
 if __name__ == "__main__":
 	go_admin = GoAdmin(IP=HOSTNAME, port=PORT, local_player=GoPlayerBase)
