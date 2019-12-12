c980250dcb43abafb28b43b9a0453995bb13d9ba
JennyTL8 <jennytl8@github.com>
2019-12-12 00:18:52 -0600

    naming

--- Deliverables/7/7.1/src/test_driver.py
+++ Deliverables/7/7.1/src/test_driver.py
@@ -1,7 +1,7 @@
 import sys, multiprocessing, time, socket, json
 sys.path.append('./../../3/3.1/src/')
 sys.path.append('./../../4/4.1/src/')
-sys.path.append('./../../8/8.1/')
+sys.path.append('./../../8/8.1/src/')
 from stone import Stone, make_stone
 from json_parser import json_parse_stdin
 from output_formatter import format_board
